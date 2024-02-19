202e2df8caadeedd535e73696045e5d5ec870cb8
kazi-kabir <kazi.s221999@gmail.com>
2024-02-20 08:44:17 +1100

    initial react practice

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -1,9 +1,27 @@
 import './App.css'
 
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Hello {props.name} you are {props.age} years old</p>
+    </div>
+  )
+}
+
+
+
 function App() {
 
+  const name = 'Peter'
+  const age = '10'
+
   return (
-    <div>Hello World</div>
+    <div>
+      <h1>Greetings</h1>
+      <Hello name='Maya' age={26+10}/>
+      <Hello name={name} age={age}/> 
+    </div>
   )
 }
 
